947ef467d45aab682704c93a936e7f6b9a8d41af
Trevor Rosen <Trevor_Rosen@rapid7.com>
2012-01-19 14:17:06 -0600

    Fixed minor issue

--- lib/msf_models/active_record_models/tag.rb
+++ lib/msf_models/active_record_models/tag.rb
@@ -2,7 +2,7 @@ module MsfModels::ActiveRecordModels::Tag
   def self.included(base)
     base.class_eval{
       include Msf::DBManager::DBSave
-      has_and_belongs_to_many :hosts, :join_table => :hosts_tags, :class_name => "Msm::Tag"
+      has_and_belongs_to_many :hosts, :join_table => :hosts_tags, :class_name => "Msm::Hosts"
 
       def to_s
         name
