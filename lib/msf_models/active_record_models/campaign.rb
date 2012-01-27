83faed58ed423e276110f1f97be30d337ac4431e
Brian Gamble <Brian_Gamble@rapid7.com>
2012-01-26 18:24:00 -0600

    Moved validation from app; rewrote validation in rails3 format

--- lib/msf_models/active_record_models/campaign.rb
+++ lib/msf_models/active_record_models/campaign.rb
@@ -37,6 +37,22 @@ module MsfModels::ActiveRecordModels::Campaign
 
       # State
       serialized_prefs_attr_accessor :error
+
+      validates_presence_of :name
+      # Don't use validates_uniqueness_of because it does a 'SELECT id ...'
+      # instead of 'SELECT *' so when we get the object in after_initialize, it
+      # only has an id attribute and trying to save it will raise
+      # ActiveRecord::MissingAttributeError.
+      # see https://rails.lighthouseapp.com/workspaces/8994/tickets/3165-activerecordmissingattributeerror-after-update-to-rails-v-234
+      #validates_uniqueness_of :name, :scope => :workspace_id
+
+      validates :web_uripath, :format => {:with => /^\//, :message => "must start with '/'"}
+
+      validates :payload_lhost, :listener_lhost,
+                :format => {:with => /^(:?\d+\.){3}\d+/, :message => "must be an IP address"}
+
+      validates_inclusion_of :smtp_port, :web_srvport, :exe_lport,
+                             :in => 1..65535, :message => "must be a number between 1 and 65535"
     }
   end
 end
