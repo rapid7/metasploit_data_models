ae69635d7edcaae4725a792ce85e0c47a5256ce6
Brian Gamble <Brian_Gamble@rapid7.com>
2012-01-27 16:40:12 -0600

    Add missing namespace

--- lib/msf_models/active_record_models/client.rb
+++ lib/msf_models/active_record_models/client.rb
@@ -1,9 +1,9 @@
 module MsfModels::ActiveRecordModels::Client
   def self.included(base)
-    base.class_eval{
+    base.class_eval {
       include Msf::DBManager::DBSave
-      belongs_to :host
-      belongs_to :campaign
+      belongs_to :host, :class_name => "Msm::Host"
+      belongs_to :campaign, :class_name => "Msm::Campaign"
     }
   end
 end
