c93ad36c6a25a9e0cc6115421b366f0ac0eb3602
Brian Gamble <Brian_Gamble@rapid7.com>
2012-01-30 16:06:17 -0600

    Msm::NexposeConsole - move validations from pro ui

--- lib/msf_models/active_record_models/nexpose_console.rb
+++ lib/msf_models/active_record_models/nexpose_console.rb
@@ -3,6 +3,12 @@ module MsfModels::ActiveRecordModels::NexposeConsole
     base.class_eval{
       include Msf::DBManager::DBSave
       serialize :cached_sites, ::MsfModels::Base64Serializer.new
+
+      validates :name, :presence => true
+      validates :address, :presence => true
+      validates :username, :presence => true
+      validates :password, :presence => true
+      validates :port, :inclusion => {:in => 1..65535}
     }
   end
 end
