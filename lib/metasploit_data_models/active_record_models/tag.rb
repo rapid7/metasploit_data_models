cb9249d7cc3d64a4eadbf7b96e414855c51e8a94
Brian Gamble <Brian_Gamble@rapid7.com>
2012-02-02 12:25:42 -0600

    Msm::Tag - move associations and validations from pro ui

--- lib/metasploit_data_models/active_record_models/tag.rb
+++ lib/metasploit_data_models/active_record_models/tag.rb
@@ -1,8 +1,15 @@
 module MetasploitDataModels::ActiveRecordModels::Tag
   def self.included(base)
-    base.class_eval{
+    base.class_eval {
       include Msf::DBManager::DBSave
-      has_and_belongs_to_many :hosts, :join_table => :hosts_tags, :class_name => "Mdm::Hosts"
+
+      has_and_belongs_to_many :hosts, :join_table => :hosts_tags, :class_name => "Mdm::Host"
+      belongs_to :user, :class_name => "Mdm::User"
+
+      validates :name, :presence => true, :format => {
+          :with => /^[A-Za-z0-9\x2e\x2d_]+$/, :message => "name must be alphanumeric, dots, dashes, or underscores"
+      }
+      validates :desc, :length => {:maximum => 8191, :message => "desc must be less than 8k."}
 
       def to_s
         name
