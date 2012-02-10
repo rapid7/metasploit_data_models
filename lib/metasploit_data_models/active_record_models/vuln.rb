4e6c4b8d6d4a1fd074da107215f9568c6a0f13d1
Brian Gamble <Brian_Gamble@rapid7.com>
2012-02-10 14:30:58 -0600

    Mdm::Vuln - move associations, validations, and callback from pro

--- lib/metasploit_data_models/active_record_models/vuln.rb
+++ lib/metasploit_data_models/active_record_models/vuln.rb
@@ -1,10 +1,21 @@
 module MetasploitDataModels::ActiveRecordModels::Vuln
   def self.included(base)
-    base.class_eval{
+    base.class_eval {
       include Msf::DBManager::DBSave
       belongs_to :host, :class_name => "Mdm::Host"
-      belongs_to :service, :class_name => "Mdm::Service"
+      belongs_to :service, :class_name => "Mdm::Service", :foreign_key => :service_id
       has_and_belongs_to_many :refs, :join_table => :vulns_refs, :class_name => "Mdm::Ref"
+
+      validates :name, :presence => true
+      validates_associated :refs
+
+      after_update :save_refs
+
+      private
+
+      def save_refs
+        refs.each { |ref| ref.save(:validate => false) }
+      end
     }
   end
 end
