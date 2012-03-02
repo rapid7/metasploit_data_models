e49990cbea2ec2d2cde73d23503c13c9fe355b94
Trevor Rosen <Trevor_Rosen@rapid7.com>
2012-03-02 17:47:48 -0600

    Remove license check from user

--- lib/metasploit_data_models/active_record_models/user.rb
+++ lib/metasploit_data_models/active_record_models/user.rb
@@ -11,24 +11,12 @@ module MetasploitDataModels::ActiveRecordModels::User
 
       validates :password, :password_is_strong => true
       validates :password_confirmation, :password_is_strong => true
-      validate { |user| available_license?(user) }
+      
 
       serialized_prefs_attr_accessor :nexpose_host, :nexpose_port, :nexpose_user, :nexpose_pass, :nexpose_creds_type, :nexpose_creds_user, :nexpose_creds_pass
       serialized_prefs_attr_accessor :http_proxy_host, :http_proxy_port, :http_proxy_user, :http_proxy_pass
       serialized_prefs_attr_accessor :time_zone, :session_key
       serialized_prefs_attr_accessor :last_login_address # specifically NOT last_login_ip to prevent confusion with AuthLogic magic columns (which dont work for serialized fields)
-
-      private
-
-      def available_license?(user)
-        if user.new_record? && license_count_exceeded?
-          user.errors[:base] << "You've reached the user limit on this license"
-        end
-      end
-
-      def license_count_exceeded?
-        self.class.count >= License.get.users
-      end
 
     }
   end
