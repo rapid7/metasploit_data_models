f434197143f44f75453037cc9738ec1314070566
Eric Thompson <Eric_Thompson@rapid7.com>
2012-04-03 10:39:31 -0500

    Removed old campaign logic from Mdm and moved exclusively to pro.

--- lib/metasploit_data_models/active_record_models/email_address.rb
+++
@@ -1,9 +0,0 @@
-module MetasploitDataModels::ActiveRecordModels::EmailAddress
-  def self.included(base)
-    base.class_eval {
-      belongs_to :campaign, :class_name => "Mdm::Campaign"
-
-      validates_uniqueness_of :address, :scope => :campaign_id
-    }
-  end
-end
