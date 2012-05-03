18dac5231c373f5244bed66be0fca472c288cb48
David Maloney <DMaloney@rapid7.com>
2012-05-03 17:07:14 -0500

    Adds an 'upgradeable' scope for sessions This returns active win32 command shell sessions that can be upgraded to meterpreter via sessions -u

--- lib/metasploit_data_models/active_record_models/session.rb
+++ lib/metasploit_data_models/active_record_models/session.rb
@@ -10,6 +10,7 @@ module MetasploitDataModels::ActiveRecordModels::Session
 
       scope :alive, where("closed_at IS NULL")
       scope :dead, where("closed_at IS NOT NULL")
+      scope :upgradeable, where("closed_at IS NULL AND stype = ? and platform ILIKE ?", "shell", "%win%")
 
       serialize :datastore, ::MetasploitDataModels::Base64Serializer.new
 
@@ -21,6 +22,7 @@ module MetasploitDataModels::ActiveRecordModels::Session
 				c = Pro::Client.get rescue nil 
         c.session_stop(self.local_id) rescue nil # ignore exceptions (XXX - ideally, stopped an already-stopped session wouldn't throw XMLRPCException)
       end
+
     }
   end
 end
