8aaa02bc54ea59ab368778cb6e93e3577a290e93
Luke Imhoff <luke_imhoff@rapid7.com>
2013-04-10 12:25:22 -0500

    Mdm::ModuleArch database specs
    [#47750371]

--- spec/app/models/mdm/module_arch_spec.rb
+++ spec/app/models/mdm/module_arch_spec.rb
@@ -5,6 +5,17 @@ describe Mdm::ModuleArch do
     it { should belong_to(:module_detail).class_name('Mdm::ModuleDetail') }
   end
 
+  context 'database' do
+    context 'columns' do
+      it { should have_db_column(:module_detail_id).of_type(:integer) }
+      it { should have_db_column(:name).of_type(:text) }
+    end
+
+    context 'indices' do
+      it { should have_db_index(:module_detail_id) }
+    end
+  end
+
   context 'factories' do
     context 'mdm_module_arch' do
       subject(:mdm_module_arch) do
