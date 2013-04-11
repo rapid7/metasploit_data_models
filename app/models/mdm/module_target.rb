6903ea0c5d42d4931971360f3c8dc17c009365a0
Luke Imhoff <luke_imhoff@rapid7.com>
2013-04-11 10:50:44 -0500

    Mdm::ModuleTarget spec
    [#47750371]

--- app/models/mdm/module_target.rb
+++ app/models/mdm/module_target.rb
@@ -5,7 +5,7 @@ class Mdm::ModuleTarget < ActiveRecord::Base
   # Associations
   #
 
-  belongs_to :module_detail
+  belongs_to :module_detail, :class_name => 'Mdm::ModuleDetail'
 
   #
   # Mass Assignment Security
@@ -18,7 +18,9 @@ class Mdm::ModuleTarget < ActiveRecord::Base
   # Validators
   #
 
-  validate :name, :presence => true
+  validates :index, :presence => true
+  validates :module_detail, :presence => true
+  validates :name, :presence => true
 
   ActiveSupport.run_load_hooks(:mdm_module_target, self)
 end
--- spec/factories/mdm/module_targets.rb
+++ spec/factories/mdm/module_targets.rb
@@ -1,6 +1,12 @@
 FactoryGirl.define do
   factory :mdm_module_target, :class => Mdm::ModuleTarget do
+    index { generate :mdm_module_target_index }
+    name { generate :mdm_module_target_name }
 
+    #
+    # Associations
+    #
+    association :module_detail, :factory => :mdm_module_detail
   end
 
   sequence :mdm_module_target_index do |n|
