e87cd59d94a321c4b52e0e0ee9cb67c8fdaa0df8
David Maloney <DMaloney@rapid7.com>
2013-05-24 23:42:40 -0500

    Add associations to hosttag spec
    Mdm::HostTag spec didn't test associations
    Added appropriate specs
    not much to see here

--- spec/app/models/mdm/host_tag_spec.rb
+++ spec/app/models/mdm/host_tag_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe Mdm::HostTag do
+
+  context 'associations' do
+    it { should belong_to(:host).class_name('Mdm::Host') }
+    it { should belong_to(:tag).class_name('Mdm::Tag') }
+  end
+
   context 'factories' do
     context 'mdm_host_tag' do
       subject(:mdm_host_tag) do
