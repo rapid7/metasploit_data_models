19869395cf4d0a25d219306caa870c4071b7a464
David Maloney <DMaloney@rapid7.com>
2013-06-02 12:31:10 -0500

    copypasta mistake

--- spec/factories/mdm/fingerprints/nmap_fingerprints.rb
+++ spec/factories/mdm/fingerprints/nmap_fingerprints.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
-  factory :mdm_session_fingerprint, :parent => :mdm_note do
+  factory :mdm_nmap_fingerprint, :parent => :mdm_note do
     ntype { 'host.os.nmap_fingerprint' }
     data { { :os_accuracy => 100, :os_family=> 'Windows',  :os_vendor=> 'Microsoft', :os_version => 'XP' } }
   end
