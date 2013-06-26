7b32bb1d57769b388a97fa0a2d07a2b68d537d51
Luke Imhoff <luke_imhoff@rapid7.com>
2013-06-26 11:15:02 -0500

    Mdm::HostDetail docs
    [#50752269]

--- app/models/mdm/host_detail.rb
+++ app/models/mdm/host_detail.rb
@@ -1,9 +1,52 @@
+# Details supplied by Nexpose about a {Mdm::Host host}.
 class Mdm::HostDetail < ActiveRecord::Base
   #
-  # Relations
+  # Associations
   #
 
+  # @!attribute [rw] host
+  #   Host that this detail is about.
+  #
+  #   @return [Mdm::Host]
   belongs_to :host, :class_name => 'Mdm::Host', :counter_cache => :host_detail_count
+
+  #
+  # Attributes
+  #
+
+  # @!attribute [rw] nx_console_id
+  #   The ID of the Nexpose console.
+  #
+  #   @return [Integer]
+
+  # @!attribute [rw] nx_device_id
+  #   The ID of the Device in Nexpose.
+  #
+  #   @return [Integer]
+
+  # @!attribute [rw] nx_risk_score
+  #   Risk score assigned by Nexpose.  Useful to ordering hosts to determine which host to target first in metasploit.
+  #
+  #   @return [Float]
+
+  # @!attribute [rw] nx_scan_template
+  #   The template used by Nexpose to perform the scan on the {#nx_site_name site} on {#host}.
+  #
+  #   @return [String]
+
+  # @!attribute [rw] nx_site_importance
+  #   The importance of scanning the {#nx_site_name site} running on {#host} according to Nexpose.
+  #
+  #   @return [String]
+
+  # @!attribute [rw] nx_site_name
+  #   Name of site running on {#host} according to Nexpose.
+  #
+  #   @return [String]
+
+  # @!attribute [rw] src
+  #    @return [String]
+  #    @todo https://www.pivotaltracker.com/story/show/52399319
 
   #
   # Validations
