c6eb959c95e7aabe0aa248c638567166fdc2d180
Luke Imhoff <luke_imhoff@rapid7.com>
2013-06-27 07:49:40 -0500

    Mdm::SessionEvent docs
    [#50752269]

--- app/models/mdm/session_event.rb
+++ app/models/mdm/session_event.rb
@@ -1,9 +1,44 @@
+# Events that occurred when using a {#session}.
 class Mdm::SessionEvent < ActiveRecord::Base
   #
-  # Relations
+  # Associations
   #
 
+  # @!attribute [rw] session
+  #   The session in which the event occured.
+  #
+  #   @return [Mdm::Session]
   belongs_to :session, :class_name => 'Mdm::Session'
+
+  # @!attribute [rw] command
+  #   The command that was run through the session that triggered this event.
+  #
+  #   @return [String]
+
+  # @!attribute [rw] created_at
+  #   When this event occurred.
+  #
+  #   @return [DateTime]
+
+  # @!attribute [rw] etype
+  #   The type of the event.
+  #
+  #   @return [String]
+
+  # @!attribute [rw] local_path
+  #   The current local directory when {#command} was run.
+  #
+  #   @return [String]
+
+  # @!attribute [rw] output
+  #   The {#output} of running {#command}.
+  #
+  #   @return [String]
+
+  # @!attribute [rw] remote_path
+  #   The current remote directory when {#command} was run.
+  #
+  #   @return [Stirng]
 
   ActiveSupport.run_load_hooks(:mdm_session_event, self)
 end
