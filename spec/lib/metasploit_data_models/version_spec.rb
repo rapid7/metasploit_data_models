b66a25d633e51ef75eca19112c8c13c1f8a435e3
Luke Imhoff <luke_imhoff@rapid7.com>
2014-07-07 12:22:00 -0500

    Fix version_spec.rb to work with tag builds on travis-ci
    MSP-10654

--- spec/lib/metasploit_data_models/version_spec.rb
+++ spec/lib/metasploit_data_models/version_spec.rb
@@ -60,7 +60,7 @@ describe MetasploitDataModels::Version do
               expect(prerelease).to eq(match[:prerelease])
             end
           else
-            tag_regex = /\Av(?<major>\d+).(?<minor>\d+).(?<patch>\d+)(-(?<prerelease>.*))?\z/
+            tag_regex = /\Av(?<major>\d+).(?<minor>\d+).(?<patch>\d+)(\.pre\.(?<prerelease>.*))?\z/
             # travis-ci sets TRAVIS_BRANCH to the tag name for tag builds
             match = branch.match(tag_regex)
 
@@ -68,8 +68,8 @@ describe MetasploitDataModels::Version do
               tag_prerelease = match[:prerelease]
 
               if tag_prerelease
-                it 'matches the tag prerelease' do
-                  expect(prerelease).to eq(tag_prerelease)
+                it 'matches the tag prerelease converted from a gem version to a VERSION' do
+                  expect(prerelease).to eq(tag_prerelease.gsub('.pre.', '-'))
                 end
               else
                 it 'does not have a PRERELEASE' do
