ff0141af9eaff9887d51fdd5fd6f91c4b7de8c44
Luke Imhoff <luke_imhoff@rapid7.com>
2014-07-16 11:50:30 -0500

    Upate version for branch
    MSP-10035

--- lib/metasploit_data_models/version.rb
+++ lib/metasploit_data_models/version.rb
@@ -4,9 +4,11 @@ module MetasploitDataModels
     # The major version number.
     MAJOR = 0
     # The minor version number, scoped to the {MAJOR} version number.
-    MINOR = 18
+    MINOR = 19
     # The patch number, scoped to the {MINOR} version number.
     PATCH = 0
+    # The prerelease version, scoped to the {PATCH} version number.
+    PRERELEASE = 'mdm-service-text'
 
     # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the {PRERELEASE} in the
     # {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0} format.
