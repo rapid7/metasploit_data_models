97f32df513fb7f84c609502bb772de97416a3455
Brandon Turner <brandon_turner@rapid7.com>
2014-07-16 10:52:23 -0500

    Update version for branch name

--- lib/metasploit_data_models/version.rb
+++ lib/metasploit_data_models/version.rb
@@ -7,6 +7,9 @@ module MetasploitDataModels
     MINOR = 18
     # The patch number, scoped to the {MINOR} version number.
     PATCH = 0
+    # The prerelease name of the given {MAJOR}.{MINOR}.{PATCH} version number. Will not be defined on master.
+    PRERELEASE = "gem-version"
+
 
     # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the {PRERELEASE} in the
     # {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0} format.
