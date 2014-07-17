bc741bfbd76cab5a02d954e3aaf6c872255cb3e6
Luke Imhoff <luke_imhoff@rapid7.com>
2014-07-17 08:23:00 -0500

    Remove bypassed method
    MSP-10033
    
    Metasploit::Model::Search::Operator::Single#type only needs to be
    overridden to automatically derive #operation_class_name, but
     #operation_class_name itself is overridden, so #type can remain
    unimplemented.

--- app/models/metasploit_data_models/search/operator/ip_address.rb
+++ app/models/metasploit_data_models/search/operator/ip_address.rb
@@ -30,9 +30,4 @@ class MetasploitDataModels::Search::Operator::IPAddress < Metasploit::Model::Sea
   def operation_class_name
     @operation_class_name ||= 'MetasploitDataModels::Search::Operation::IPAddress'
   end
-
-  # @return [Symbol] `:ip_address`
-  def type
-    :ip_address
-  end
 end
