231a9df9a9c622e92515289fc6a81d758a855b18
lsanchez-r7 <lance.sanchez@rapid7.com>
2014-09-05 11:44:40 -0500

    updating the PRERELEASE branch name

--- lib/metasploit_data_models/version.rb
+++ lib/metasploit_data_models/version.rb
@@ -8,7 +8,7 @@ module MetasploitDataModels
     # The patch number, scoped to the {MINOR} version number.
     PATCH = 0
     
-    PRERELEASE = 'rails-4.0'
+    PRERELEASE = 'ip-validation'
 
     # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the `PRERELEASE` in the
     # {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0} format.
