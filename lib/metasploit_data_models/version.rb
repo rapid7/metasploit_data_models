346a9e085a5da4f458e6484d5c4d1c3dc2830a73
Samuel Huckins <samuel_huckins@rapid7.com>
2014-12-19 20:16:01 -0600

    Lands #83, drop NX console addr proto before save

--- lib/metasploit_data_models/version.rb
+++ lib/metasploit_data_models/version.rb
@@ -8,8 +8,6 @@ module MetasploitDataModels
     # The patch number, scoped to the {MINOR} version number.
     PATCH = 3
 
-    PRERELEASE = 'nexpose-console-address'
-
     # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the `PRERELEASE` in the
     # {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0} format.
     #
