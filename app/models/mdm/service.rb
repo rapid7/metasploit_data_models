4c6e45e3a6d6ebd1301d6fee6eb8cc982172facb
Samuel Huckins <samuel_huckins@rapid7.com>
2015-01-28 12:46:59 -0600

    Inverting port/proto validation
    MSP-11643
    
    * We now verify the combination of port and protocol within the scope
    of the parent host ID, versus verifying host_id is unique
    within the scope of port/proto. This allows the validation messages to
    have somewhere to appear in the service add window, associated with the
    port and protocol fields.

--- app/models/mdm/service.rb
+++ app/models/mdm/service.rb
@@ -222,14 +222,19 @@ class Mdm::Service < ActiveRecord::Base
                 in: PROTOS
             }
 
-  validates :host_id,
-            uniqueness: {
-              message: 'already has a service with this port and proto',
-              scope: [
-                :port,
-                :proto
-              ]
-            }
+  validate :uniq_port_proto_per_host
+
+  # Custom validation to ensure that the combination of port and protocol
+  # for a service doesn't already exist for the same host.
+  def uniq_port_proto_per_host
+    no_dupes = Mdm::Service.where(host_id: host.id, port: port, proto: proto).blank?
+    error_msg = 'This host already has a service with this port and protocol.'
+
+    unless no_dupes
+      errors.add(:port, error_msg)
+      errors.add(:proto, error_msg)
+    end
+  end
 
   #
   # Class Methods
