e130b078140b721a826a1b11e113be95ec7ffeb5
James Lee <egypt@metasploit.com>
2015-02-26 15:02:24 -0600

    Fix prerelease

--- lib/metasploit_data_models/version.rb
+++ lib/metasploit_data_models/version.rb
@@ -8,7 +8,7 @@ module MetasploitDataModels
     # The patch number, scoped to the {MINOR} version number.
     PATCH = 1
 
-    PRERELEASE='move-auto-exploitation-match'
+    PRERELEASE='automatic-exploitation-migration'
 
     # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the `PRERELEASE` in the
     # {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0} format.
