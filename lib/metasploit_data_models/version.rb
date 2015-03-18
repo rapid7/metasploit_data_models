85f12581bed55f128ddc18645316b969098fea12
Matt Buck <Matthew_Buck@rapid7.com>
2015-03-18 15:19:16 -0500

    Updating prelease version

--- lib/metasploit_data_models/version.rb
+++ lib/metasploit_data_models/version.rb
@@ -8,7 +8,7 @@ module MetasploitDataModels
     # The patch number, scoped to the {MAJOR} and {MINOR} version numbers.
     PATCH = 1
 
-    PRERELEASE = 'rename-indices'
+    PRERELEASE = 'rails-4.0'
 
     # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the `PRERELEASE` in the
     # {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0} format.
