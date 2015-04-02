1413dc6e317653802f5408fd1d1fe7bbfd5891a0
Trevor Rosen <trevor_rosen@rapid7.com>
2015-04-02 13:47:37 -0500

    Remove PRERELEASE

--- lib/metasploit_data_models/version.rb
+++ lib/metasploit_data_models/version.rb
@@ -7,8 +7,6 @@ module MetasploitDataModels
     MINOR = 23
     # The patch number, scoped to the {MAJOR} and {MINOR} version numbers.
     PATCH = 3
-    # The pre-release version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version numbers.
-    PRERELEASE = 'autoload'
 
     # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the `PRERELEASE` in the
     # {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0} format.
