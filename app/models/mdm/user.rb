cc794aab05c8c605867185349a099b2c0bbf6a1e
Fernando Arias <fernando_arias@rapid7.com>
2015-04-03 14:03:29 -0500

    Revert "Drop another attr_accessible call"
    This reverts commit c5bcead4c75126e093ac7717886105b3bb256a1e.

--- app/models/mdm/user.rb
+++ app/models/mdm/user.rb
@@ -27,6 +27,10 @@ class Mdm::User < ActiveRecord::Base
   serialized_prefs_attr_accessor :time_zone, :session_key
   serialized_prefs_attr_accessor :last_login_address # specifically NOT last_login_ip to prevent confusion with AuthLogic magic columns (which dont work for serialized fields)
 
+  # Model Associations
+
+  attr_accessible :owned_workspaces, :tags, :workspaces
+
   Metasploit::Concern.run(self)
 end
 
