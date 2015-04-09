b52d47801f388eac996f120c25f10f21a908fc92
Samuel Huckins <samuel_huckins@rapid7.com>
2015-04-09 17:09:21 -0500

    Set PRERELEASE

--- lib/metasploit_data_models/version.rb
+++ lib/metasploit_data_models/version.rb
@@ -8,6 +8,8 @@ module MetasploitDataModels
     # The patch number, scoped to the {MAJOR} and {MINOR} version numbers.
     PATCH = 3
 
+    PRERELEASE = 'set-postgres93-travis'
+
     # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the `PRERELEASE` in the
     # {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0} format.
     #
