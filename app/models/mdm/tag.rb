8ba12f83025412d609eed2bfd2549106eadb1ef4
Luke Imhoff <luke_imhoff@rapid7.com>
2015-04-23 12:40:57 -0500

    Document Mdm::Tag
    MSP-11071
    
    Ported from
    https://github.com/rapid7/metasploit_data_models/blob/f41214b1106bcbc17d6fa7327bda2b0bef50e350/app/models/mdm/tag.rb.

--- app/models/mdm/tag.rb
+++ app/models/mdm/tag.rb
@@ -1,32 +1,70 @@
+# Tag {#hosts_tags assigned} to {#hosts}.  Tags can be used to group together hosts for targeting and reporting.
 class Mdm::Tag < ActiveRecord::Base
   include Metasploit::Model::Search
 
   #
-  # Relations
+  # Associations
   #
 
-  # @!attribute hosts_tags
-  #  Joins {#hosts} to this tag.
-  #
-  #  @return [ActiveRecord::Relation<Mdm::HostTag>]
+  # Joins {#hosts} to this tag.
   has_many :hosts_tags,
            class_name: 'Mdm::HostTag',
            dependent: :destroy,
            inverse_of: :tag
 
+  # User that created this tag.
   belongs_to :user,
              class_name: 'Mdm::User',
              inverse_of: :tags
 
   #
-  # Through :hosts_tags
+  # through: :hosts_tags
   #
 
-  # @!attribute [r] hosts
-  #   Host that are tagged with this tag.
-  #
-  #   @return [ActiveRecord::Relation<Mdm::Host>]
+  # Hosts that are tagged with this tag.
   has_many :hosts, :through => :hosts_tags, :class_name => 'Mdm::Host'
+
+  #
+  # Attributes
+  #
+
+  # @!attribute created_at
+  #   When this tag was created by {#user}.
+  #
+  #   @return [DateTime]
+
+  # @!attribute critical
+  #   Whether this tag represents a critical finding about the {#hosts}.
+  #
+  #   @return [true] this tag is critical.
+  #   @return [false] this tag is non-critical.
+
+  # @!attribute desc
+  #   Longer description of what this tag should be used for or means when applied to a {#hosts host}.
+  #
+  #   @return [String]
+
+  # @!attribute name
+  #   The name of the tag.  The name is what a user actually enters to tag a {#hosts host}.
+  #
+  #   @return [String]
+
+  # @!attribute report_detail
+  #   Whether to include this tag in a report details section.
+  #
+  #   @return [true] include this tag in the report details section.
+  #   @return [false] do not include this tag in the report details section.
+
+  # @!attribute report_summary
+  #   Whether to include this tag in a report summary section.
+  #
+  #   @return [true] include this tag in the report summary section.
+  #   @return [false] do not include this tag in the report summary section.
+
+  # @!attribute updated_at
+  #   The last time this tag was updated.
+  #
+  #   @return [DateTime]
 
   #
   # Search
@@ -67,6 +105,7 @@ class Mdm::Tag < ActiveRecord::Base
     end
   end
 
+  # (see #name)
   def to_s
     name
   end
