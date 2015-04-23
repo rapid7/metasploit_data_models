2adfeb1066379b3664908d03762c3128bc8aa83e
Luke Imhoff <luke_imhoff@rapid7.com>
2015-04-23 12:08:22 -0500

    Document Mdm::Module::Target
    MSP-11071

--- app/models/mdm/module/target.rb
+++ app/models/mdm/module/target.rb
@@ -1,3 +1,5 @@
+# A potential target for a {Mdm::Module::Detail exploit Metasploit Module}.  Targets can change options including
+# offsets for ROP chains to tune an exploit to work with different system libraries and versions.
 class Mdm::Module::Target < ActiveRecord::Base
   self.table_name = 'module_targets'
 
@@ -5,7 +7,23 @@ class Mdm::Module::Target < ActiveRecord::Base
   # Associations
   #
 
+  # Exploit Metasploit Module with the {#name named} target at the given {#index}.
   belongs_to :detail, :class_name => 'Mdm::Module::Detail'
+
+  #
+  # Attributes
+  #
+
+  # @!attribute index
+  #   The index of this target in the {#detail exploit Metasploit Module}'s list of targets. The index is used for
+  #   target selection.
+  #
+  #   @return [Integer]
+
+  # @!attribute name
+  #   The name of this target.
+  #
+  #   @return [String]
 
   #
   # Mass Assignment Security
