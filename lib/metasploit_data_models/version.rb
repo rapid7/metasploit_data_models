58a0a962c9e730fe283b865d827d0ddad19c79da
Luke Imhoff <luke_imhoff@rapid7.com>
2015-04-23 08:20:24 -0500

    Update version for branch
    MSP-11071

--- lib/metasploit_data_models/version.rb
+++ lib/metasploit_data_models/version.rb
@@ -6,9 +6,9 @@ module MetasploitDataModels
     # The minor version number, scoped to the {MAJOR} version number.
     MINOR = 24
     # The patch number, scoped to the {MAJOR} and {MINOR} version numbers.
-    PATCH = 0
+    PATCH = 1
     # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version numbers.
-    # PRERELEASE =
+    PRERELEASE = 'metasploit-yard'
 
     # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the `PRERELEASE` in the
     # {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0} format.
