aef8e9e908e33573bfd692dc2e6978f47ac42aa6
Luke Imhoff <luke_imhoff@rapid7.com>
2015-04-27 14:33:33 -0500

    Document IPFormatValidator
    MSP-11071

--- app/validators/ip_format_validator.rb
+++ app/validators/ip_format_validator.rb
@@ -1,6 +1,10 @@
 require "ipaddr"
 
+# Validates that attribute is a valid IPv4 or IPv6 address.
 class IpFormatValidator < ActiveModel::EachValidator
+  # Validates that `attribute`'s `value` on `object` is a valid IPv4 or IPv6 address.
+  #
+  # @return [void]
   def validate_each(object, attribute, value)
     error_message_block = lambda{ object.errors[attribute] << " must be a valid IPv4 or IPv6 address" }
     begin
