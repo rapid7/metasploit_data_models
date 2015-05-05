93b0ba7ce55faead340bb35a3c4d0a9653cdf76c
Luke Imhoff <luke_imhoff@rapid7.com>
2015-05-05 15:59:36 -0500

    Fix typo
    MSP-12669

--- spec/app/models/mdm/host_spec.rb
+++ spec/app/models/mdm/host_spec.rb
@@ -729,7 +729,7 @@ RSpec.describe Mdm::Host, type: :model do
         stub_const(
             'Rex::Text',
             Module.new do
-              def self.ascii_safe_hex(unsantized)
+              def self.ascii_safe_hex(unsanitized)
                 # Pass back the sanitized value for the stub
                 unsanitized.unpack("C*").pack("C*").gsub(/([\x00-\x08\x0b\x0c\x0e-\x1f\x80-\xFF])/n){ |x| "\\x%.2x" % x.unpack("C*")[0]}
               end
