7ec7738b8232c7a162594cd9370e2764d488ecf1
Luke Imhoff <luke_imhoff@rapid7.com>
2015-05-06 09:23:03 -0500

    `ensure_exclusion_of` -> `validate_exclusion_of`
    MSP-12679

--- spec/app/models/mdm/host_spec.rb
+++ spec/app/models/mdm/host_spec.rb
@@ -380,7 +380,7 @@ describe Mdm::Host do
 
   context 'validations' do
     context 'address' do
-      it { should ensure_exclusion_of(:address).in_array(['127.0.0.1']) }
+      it { should validate_exclusion_of(:address).in_array(['127.0.0.1']) }
       it { should validate_presence_of(:address) }
 
       # can't use validate_uniqueness_of(:address).scoped_to(:workspace_id) because it will attempt to set workspace_id
