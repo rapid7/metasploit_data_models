cd02bcf59b6187c2688f8dbd50c03c0b3d3f0788
lsanchez-r7 <lance.sanchez@rapid7.com>
2015-05-11 16:36:45 -0500

    dropping PRERELEASE

--- lib/metasploit_data_models/version.rb
+++ lib/metasploit_data_models/version.rb
@@ -11,8 +11,6 @@ module MetasploitDataModels
     MINOR = 0
     # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
     PATCH = 0
-    # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version numbers.
-    PRERELEASE = 'rails-4.0'
 
     #
     # Module Methods
