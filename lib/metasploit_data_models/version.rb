b3c56913c8b1070b1c12c007ce7f71cc348c79ad
Sonny Gonzalez <sonny_gonzalez@rapid7.com>
2015-05-18 14:30:15 -0500

    Polymorphic relationship between Mdm::Vuln and it's origin
    MSP-12395
    
    - add comment to PRERELEASE constant to fix Travis failure

--- lib/metasploit_data_models/version.rb
+++ lib/metasploit_data_models/version.rb
@@ -12,6 +12,7 @@ module MetasploitDataModels
     # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
     PATCH = 0
 
+    # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version numbers.
     PRERELEASE = 'polymorphic-vuln-import'
     #
     # Module Methods
