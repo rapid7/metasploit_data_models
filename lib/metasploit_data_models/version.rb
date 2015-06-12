540fc0593de23f38e22e93187235f60c9aee595e
Sonny Gonzalez <sonny_gonzalez@rapid7.com>
2015-06-12 13:17:58 -0500

    Remove PRERELEASE
    MSP-12813

--- lib/metasploit_data_models/version.rb
+++ lib/metasploit_data_models/version.rb
@@ -11,8 +11,6 @@ module MetasploitDataModels
     MINOR = 2
     # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
     PATCH = 2
-    # Remove on master
-    PRERELEASE = 'matches-killed-on-detail-delete'
 
     #
     # Module Methods
