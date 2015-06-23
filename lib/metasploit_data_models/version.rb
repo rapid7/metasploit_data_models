fc98afb291f6e56fd17e45141ba349be029719a7
Samuel Huckins <samuel_huckins@rapid7.com>
2015-06-23 10:57:23 -0500

    Dropping PRERELEASE post PR 134 merge

--- lib/metasploit_data_models/version.rb
+++ lib/metasploit_data_models/version.rb
@@ -11,8 +11,6 @@ module MetasploitDataModels
     MINOR = 2
     # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
     PATCH = 5
-    # Remove on master
-    PRERELEASE = 'crawler-choke-on-save'
 
 
     #
