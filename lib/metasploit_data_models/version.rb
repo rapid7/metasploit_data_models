b6c85052322e5357fdc2260993a75c257efcdcfb
Brent Cook <bcook@rapid7.com>
2015-10-22 16:02:19 -0700

    Remove PRERELEASE

--- lib/metasploit_data_models/version.rb
+++ lib/metasploit_data_models/version.rb
@@ -11,8 +11,6 @@ module MetasploitDataModels
     MINOR = 2
     # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
     PATCH = 9
-    # Remove on master
-    PRERELEASE = 'search-host-name-vulns'
 
     #
     # Module Methods
