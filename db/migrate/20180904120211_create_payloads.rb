54042dcc7086d88d54c0e138b53e5d3eb5da911d
James Barnett <James_Barnett@rapid7.com>
2018-09-06 16:59:35 -0500

    Remove UUID generation

--- db/migrate/20180904120211_create_payloads.rb
+++ db/migrate/20180904120211_create_payloads.rb
@@ -1,14 +1,16 @@
 class CreatePayloads < ActiveRecord::Migration
   def change
     create_table :payloads do |t|
-      t.string :puid
       t.string :name
+      t.string :uuid
       t.boolean :registered
       t.integer :timestamp
       t.string :arch
       t.string :platform
-      t.integer :xor1
-      t.integer :xor2
+      t.string :urls
+      t.string :description
+      t.references :workspace
+
 
       t.timestamps null: false
     end
