33d95a31cde0257a3bfd74126dace1255e6e8cef
dwelch-r7 <dean_welch@rapid7.com>
2021-05-05 14:52:46 +0100

    Add ruby 3.0 to CI

--- db/migrate/20140905031549_add_detected_arch_to_host.rb
+++ db/migrate/20140905031549_add_detected_arch_to_host.rb
@@ -1,5 +1,5 @@
 class AddDetectedArchToHost < ActiveRecord::Migration[4.2]
   def change
-    add_column :hosts, :detected_arch, :string, { :null => true }
+    add_column :hosts, :detected_arch, :string, :null => true
   end
 end
